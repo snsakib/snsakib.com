bdde00cb32b7935985e8b772155f605cfd8b51f4
snsakib <hello@snsakib.com>
2024-01-16 07:53:07 +0600

    Draft Home Page Brief Intro

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import BriefIntro from '@/components/home/BriefIntro'
 import Hero from '@/components/home/Hero'
 
 export default function Home() {
   return (
     <main>
-      <Hero></Hero>
+      <Hero/>
+      <BriefIntro/>
     </main>
   )
 }
---
+++ components/home/BriefIntro.js
@@ -0,0 +1,37 @@
+export default function BriefIntro() {
+  return (
+    <section id="home-brief-intro">
+      <h2>A Little Bit of Myself</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nec ultrices dui
+        sapien eget mi proin. Pellentesque elit ullamcorper dignissim cras
+        tincidunt lobortis feugiat. Mauris ultrices eros in cursus turpis massa
+        tincidunt dui ut. Est ultricies integer quis auctor elit sed vulputate
+        mi sit. Est ante in nibh mauris cursus mattis. Elit pellentesque
+        habitant morbi tristique. Imperdiet proin fermentum leo vel orci porta
+        non. Donec et odio pellentesque diam volutpat commodo sed. Ut tortor
+        pretium viverra suspendisse potenti nullam ac tortor vitae. Elementum
+        nibh tellus molestie nunc non. Neque vitae tempus quam pellentesque nec
+        nam. Ut morbi tincidunt augue interdum. Amet tellus cras adipiscing enim
+        eu turpis. Imperdiet massa tincidunt nunc pulvinar sapien. Elementum
+        integer enim neque volutpat ac tincidunt vitae semper.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nec ultrices dui
+        sapien eget mi proin. Pellentesque elit ullamcorper dignissim cras
+        tincidunt lobortis feugiat. Mauris ultrices eros in cursus turpis massa
+        tincidunt dui ut. Est ultricies integer quis auctor elit sed vulputate
+        mi sit. Est ante in nibh mauris cursus mattis. Elit pellentesque
+        habitant morbi tristique. Imperdiet proin fermentum leo vel orci porta
+        non. Donec et odio pellentesque diam volutpat commodo sed. Ut tortor
+        pretium viverra suspendisse potenti nullam ac tortor vitae. Elementum
+        nibh tellus molestie nunc non. Neque vitae tempus quam pellentesque nec
+        nam. Ut morbi tincidunt augue interdum. Amet tellus cras adipiscing enim
+        eu turpis. Imperdiet massa tincidunt nunc pulvinar sapien. Elementum
+        integer enim neque volutpat ac tincidunt vitae semper.
+      </p>
+    </section>
+  );
+}
