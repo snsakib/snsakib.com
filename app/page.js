df18f93020b35075eb1ab70abece419df2466582
snsakib <hello@snsakib.com>
2024-05-12 15:27:12 +0600

    home clients section divider added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Home/Hero";
 import HomeClients from "@/components/Home/Clients";
+import HomeProjects from "@/components/Home/Projects";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <HomeClients />
+      <HomeProjects />
     </main>
   );
 }
---
+++ components/Home/Projects.js
@@ -0,0 +1,10 @@
+export default function HomeProjects() {
+  return (
+    <section
+      id="home-projects"
+      className="min-h-dvh page-section border border-black"
+    >
+      <h1>Hello</h1>
+    </section>
+  );
+}
