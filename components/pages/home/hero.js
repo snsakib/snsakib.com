ce32fa6a45551e07bcc6053f338c0555d27e3cbb
snsakib <hello@snsakib.com>
2025-07-03 10:00:46 +0600

    Update dependencies and styles
    - Updated gsap dependency from 3.12.7 to 3.13.0 in package.json.
    - Modified hero.css to change alignment from center to start and updated opacity for hero image and title to 1.
    - Added new font files: HikouOutline.otf and PorticoOutline.woff.

--- components/pages/home/hero.js
+++ components/pages/home/hero.js
@@ -1,86 +1,29 @@
 "use client";
-import { useRef } from 'react';
+import { portico  } from "@/app/fonts-loader";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { TextPlugin } from "gsap/TextPlugin";
 
-gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(TextPlugin) 
 
 export default function HomeHero() {
-  const imgRef = useRef(null);
-  const textRef = useRef(null);
 
   useGSAP(() => {
-    
-    gsap.fromTo(
-      imgRef.current, 
-      {
-        y: -100,
-        opacity: 0
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        ease: "power1.out"
-      }
-    );
+    const tl = gsap.timeline({ repeat: -1, delay: 1.5, repeatDelay: 1.5, yoyo: true });
 
-    gsap.fromTo(
-      textRef.current, 
-      {
-        y: 100,
-        opacity: 0
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        ease: "power1.out"
-      }
-    )
-
-    gsap.fromTo(
-      '.contact-link', 
-      {
-        y: 50,
-        opacity: 0
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 0.5,
-        delay: 0.5,
-        stagger: 0.1,
-        ease: "power1.out"
-      }
-    )
-
-    const t1 = gsap.timeline({
-      repeat: -1,
-      delay: 2,
-      repeatDelay: 3
+    tl.to("#home-hero-service", {
+      duration: 1.5,
+      text: "Blockchain",
+      ease: "power2.inOut",
     });
-
-    t1.to(textRef.current, {
-      duration: 3,
-      text: "Blockchain Developer"
-    });
-
-    t1.to(textRef.current, {
-      duration: 3,
-      text: "Full Stack Web Developer",
-      delay: 3
-    })
   }, []);
 
   return (
     <section className="hero-wrapper">
       <div id="home-hero" className="page-section">
-        <img ref={imgRef} id="home-hero-img" src="/assets/img/home-hero-name.svg" />
-        <h2 id="home-hero-title" ref={textRef} className="text-xl md:text-3xl xl:text-4xl">
-          Full Stack Web Developer
-        </h2>
+        <p className={ portico.className + " text-2xl md:text-3xl xl:text-5xl"}>Hi, I'm</p>
+        <img id="home-hero-img" src="/assets/img/home-hero-name.svg" />
+        <p className={"text-xl md:text-2xl xl:text-4xl"}>Helping businesses through <span id="home-hero-service" className="text-gradient-1">Web Development</span> solutions</p>
       </div>
     </section>
   );
